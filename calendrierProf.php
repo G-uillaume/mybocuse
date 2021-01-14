a87c5284941cd9f7b5edad03d536f49c4b144fae
Jniouen-Wassim <jniouenwassim@gmail.com>
2021-01-14 11:44:23 +0100

    commit

--- calendrierProf.php
+++ calendrierProf.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/9be8d195b1.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
     <link rel="stylesheet" href="style.css">
 
     <title>My.Beaucuz</title>
@@ -43,18 +44,18 @@
     ?>
 
     <!------------------------------- Historique ------------------------------------------>
-    <section class="historique">
+    <!-- <section class="historique">
 
         <button type="button" class="btn btn-dark">Historique <i class="fas fa-align-center"></i></button>
 
-    </section>
+    </section> -->
 
     <!------------------------------- Calendrier ------------------------------------------>
 
 
     <div class="container">
-        <div class="row mx-auto">
-            <div class="col-5" style="height:100vh; border: 1px solid black; border-radius: 5px;">
+        <div class="tableauProf row mt-5">
+            <div class="col-5">
 
                 <table class="tableRecipe table">
                     <thead>
@@ -63,7 +64,7 @@
                             <th></th>
                             <th>Nom</th>
                             <th>Date</th>
-                            <th>Recette</th>                        
+                            <th>Recette</th>
                         </tr>
 
                         <tr>
@@ -106,45 +107,112 @@
                     </tbody>
 
                 </table>
+                <!------------------------------- Button Ajouté ------------------------------------------>
+
+                <div class="ajouter">
+                    <button type="button" class="btn btn-dark" data-bs-toggle="modal"
+                        data-bs-target="#recipesProfModal">Ajouter <i class="fas fa-plus-circle"></i></button>
+                </div>
+
+                <!-- Modal -->
+                <div class="modal fade" id="recipesProfModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Add a recipe watch</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <?php include('./includes/recipe_form.php')?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!------------------------------- Button Ajouté ------------------------------------------>
             </div>
 
             <div class="col sm-1"></div>
 
 
-            <div class="col-5" style="background-color: yellow; height:100vh; border-radius: 5px;">salut</div>
+            <div class="col-5">
 
+                <table class="tableauCheck table">
+                    <thead>
 
-        </div>
-    </div>
+                        <tr>
+                            <th></th>
+                            <th>Nom</th>
+                            <th>Date</th>
+                            <th>Recette</th>
+                        </tr>
 
-    <!------------------------------- Button Ajouté ------------------------------------------>
+                        <tr>
+                            <th scope="col">Monday </th>
+                            <td>Mark</td>
+                            <td>14/01/2021</td>
+                            <td>Gateau au Canard</td>
 
-    <div class="ajouter">
-        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#recipesProfModal">Ajouter <i
-                class="fas fa-plus-circle"></i></button>
-    </div>
+                        </tr>
+                    </thead class="jourSemaine">
+                    <tbody>
+                        <tr>
+                            <th scope="col">Tuesday</th>
+                            <td>Nom</td>
+                            <td>Date</td>
+                            <td>Recette</td>
+                        </tr>
 
-    <!-- Modal -->
-    <div class="modal fade" id="recipesProfModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Add a recipe watch</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <tr>
+                            <th scope="col">Wednesday</th>
+                            <td>Nom</td>
+                            <td>Date</td>
+                            <td>Recette</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="col">Thursday</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="col">Friday</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+
+                    </tbody>
+
+                </table>
+                <!------------------------------- Button Selected ------------------------------------------>
+
+                <div class="dropdown">
+                    <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
                 </div>
-                <div class="modal-body">
-                    <?php include('./includes/recipe_form.php')?>
-                </div>
+                <!------------------------------- Button Selected ------------------------------------------>
             </div>
+
+
         </div>
     </div>
 
 
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
     </script>
+
 </body>
 
 </html>
